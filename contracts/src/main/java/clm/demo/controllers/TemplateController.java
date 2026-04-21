3ae35870e99ed429dbf3adb26f63d8714dde9824
Gabriel Cărăuleanu <gabrielvalentine738@gmail.com>
2026-04-21 20:33:27 +0300

    last conflict

--- contracts/src/main/java/clm/demo/controllers/TemplateController.java
+++ contracts/src/main/java/clm/demo/controllers/TemplateController.java
@@ -5,6 +5,7 @@ import clm.demo.dto.requests.UploadTemplateRequest;
 import clm.demo.dto.responses.TemplateUploadResponseDTO;
 import clm.demo.dto.responses.TemplateFieldResponseDTO;
 import clm.demo.dto.responses.TemplateResponseDTO;
+import clm.demo.dto.responses.AvailableFieldResponseDTO;
 import clm.demo.models.enums.DocumentFormat;
 import clm.demo.models.enums.DocumentType;
 import clm.demo.services.TemplateService;
