219f55b1feff899f844d6386aac1b0f13cf6e939
Miruna Andreea Stefan <you@example.com>
2026-05-10 19:43:02 +0300

    Setup pentru inceperea lucrului la microserviciul de negocieri

---
+++ services/negotiation-service/src/main/java/clm/negotiation/NegotiationApplication.java
@@ -0,0 +1,11 @@
+package clm.negotiation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NegotiationApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NegotiationApplication.class, args);
+    }
+}
