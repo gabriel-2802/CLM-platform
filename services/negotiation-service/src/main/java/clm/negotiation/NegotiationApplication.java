76ce4c084d0c57b0f10db34d1e7aa43524c24422
Miruna Andreea Stefan <you@example.com>
2026-05-11 01:30:25 +0300

    Am pregatit infrastructura necesara pentru a putea trimite mail-uri din microserviciul de negocieri: Am adaugat mail dependency si scheduling - scopul: separarea mail-urilor care notifica apropierea expirarii unui contract de mail-urile care afiseaza un digest al clientilor cu care nu s-a mai negociat de 6 luni

--- services/negotiation-service/src/main/java/clm/negotiation/NegotiationApplication.java
+++ services/negotiation-service/src/main/java/clm/negotiation/NegotiationApplication.java
@@ -2,8 +2,10 @@ package clm.negotiation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class NegotiationApplication {
     public static void main(String[] args) {
         SpringApplication.run(NegotiationApplication.class, args);
